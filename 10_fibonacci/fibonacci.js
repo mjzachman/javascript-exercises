adf874237a65efd7ddd0bb59f01abe3f74712cda
Matt Zachman <mzachman14@gmail.com>
2022-12-06 15:41:31 -0500

    complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {return "OOPS";}
+    if (typeof index === "string"){index = Number(index);}
+
+    let lastTwo = [0,1];
+    let fibSum = 0;
+
+    for (let i = 0; i < index; i++){
+        fibSum = lastTwo[0] + lastTwo[1];
+        lastTwo.pop();
+        lastTwo.unshift(fibSum);
+    }
+    
+    return fibSum;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
