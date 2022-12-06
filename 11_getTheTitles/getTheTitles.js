a968eaa99daf73afedf5efa159d849f27f23c6e3
Matt Zachman <mzachman14@gmail.com>
2022-12-06 15:53:16 -0500

    complete titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let newArr = [];
 
+    for (let i = 0; i < arr.length; i++){
+        newArr.push(arr[i].title);
+        console.log(newArr);
+    }
+
+    return newArr;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
