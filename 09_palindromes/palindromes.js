0c2d5ebcf8da3981465a5a4e3b6b5dc5d7e8a73f
Matt Zachman <mzachman14@gmail.com>
2022-12-06 15:12:19 -0500

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const regex = /[a-z]/g
+    string = string.toLowerCase();
+    let arr = string.match(regex);
+    //let arr = string.split('');
 
+    for (let i = string.length - 1; i > 0; i--){
+       if(arr[0] === arr[i]){
+        arr.shift();
+        arr.pop();
+       }
+    }
+
+    if(arr.length === 1 || arr.length === 0){return true;}
+    else {return false;}
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
